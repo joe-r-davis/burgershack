1885256646ee98be692e49587b40a405586c2689
Jake Overall <joverall22@gmail.com>
2018-04-03 16:45:18 -0600

    wow auth is cool right?

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using burger_shack.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,43 +15,70 @@ using MySql.Data.MySqlClient;
 
 namespace burger_shack
 {
-    public class Startup
+  public class Startup
+  {
+    private readonly string _connectionString = "";
+    public Startup(IConfiguration configuration)
     {
-        private readonly string _connectionString = "";
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-            _connectionString = configuration.GetSection("DB").GetValue<string>("mySQLConnectionString");
-        }
-
-        public IConfiguration Configuration { get; }
-
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-            services.AddTransient<IDbConnection>(x => CreateDBContext());
-            services.AddTransient<BurgerRepository>();
-        }
-
-        private IDbConnection CreateDBContext()
-        {
-            var connection = new MySqlConnection(_connectionString);
-            connection.Open();
-            return connection;
-        }
-
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
-        }
+      Configuration = configuration;
+      _connectionString = configuration.GetSection("DB").GetValue<string>("mySQLConnectionString");
     }
+
+    public IConfiguration Configuration { get; }
+
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services
+      .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+      .AddCookie(o =>
+      {
+        o.LoginPath = "/Account/Login/";
+        o.Events.OnRedirectToLogin = (context) =>
+        {
+          context.Response.StatusCode = 401;
+          return Task.CompletedTask;
+        };
+      });
+
+      services.AddCors(o =>
+      {
+        o.AddPolicy("CORS_ENV_DEVELOPMENT", builder =>
+        {
+          builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+        });
+      });
+
+
+
+      services.AddMvc();
+      services.AddTransient<IDbConnection>(x => CreateDBContext());
+      services.AddTransient<UserRepository>();
+      services.AddTransient<BurgerRepository>();
+    }
+
+    private IDbConnection CreateDBContext()
+    {
+      var connection = new MySqlConnection(_connectionString);
+      connection.Open();
+      return connection;
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseCors("CORS_ENV_DEVELOPMENT");
+      }
+
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+
+      app.UseAuthentication();
+      app.UseMvc();
+    }
+  }
 }
--- burger-shack-sql-setup.sql
+++ burger-shack-sql-setup.sql
@@ -34,4 +34,13 @@
 
 -- DROP TABLE burgers;
 
+-- DROP TABLE users;
 
+-- CREATE TABLE users (
+--   id VARCHAR(255) NOT NULL,
+--   name VARCHAR(255),
+--   email VARCHAR(255) NOT NULL,
+--   password VARCHAR(255) NOT NULL,
+--   PRIMARY KEY (id),
+--   UNIQUE KEY email (email)
+-- )
